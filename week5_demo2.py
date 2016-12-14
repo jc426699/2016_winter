d52c3265fec399cc4f42608f73f92bf43ff969f1
yokyen <yokyen.nguwi@jcu.edu.au>
2016-12-14 11:37:50 +0800

    Added week 4 demo codes

---
+++ week5_demo2.py
@@ -0,0 +1,36 @@
+words = {}
+words["avalanche"] = """
+1. a large mass of snow, ice, etc., detached from a mountain slope and sliding or falling suddenly downward.
+2.anything like an avalanche in suddenness and overwhelming quantity:
+an avalanche of misfortunes; an avalanche of fan mail.
+3.Also called Townsend avalanche. Physics, Chemistry. a cumulative ionization process in which the ions and electrons of one generation undergo collisions that produce a greater number of ions and electrons in succeeding generations.
+"""
+
+#plants = {} # key is name, value is the color
+plants = {"carrot":"orange",
+          "raddish": "white",
+          "beetroot": "red"
+          }
+plants["mango"] = "yellow"
+
+for each in plants: #return the key
+    print("Getting plant:{}, value: {}".format(each, plants[each]))
+#print(plants)
+
+#print(plants.items())
+for key, value in plants.items():
+    print("{:10s} -> {:10s}".format(key, value))
+
+for value in plants.values():
+    print(value)
+
+names = ["John", "John", "Ann", "Jane"] #list of names
+#count the occurrence of each name
+count = {} #John = 2
+
+for each in names:
+    if each in count: #if it exist in count dict
+        count[each] += 1
+    else: #first time
+        count[each] = 1
+    print(count)
